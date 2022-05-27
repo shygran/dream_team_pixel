7fef03fd7f1d376e410bd3e0c3e612a9dc528dbd
Sanzharmoldobekov <103410435+Sanzharmoldobekov@users.noreply.github.com>
2022-05-27 16:52:11 +0600

    Update hakaton4.py

--- hakaton4.py
+++ hakaton4.py
@@ -171,22 +171,52 @@ cursor = connection.cursor()
 # cursor.execute(sulpak_insert)
 # connection.commit()
 
+# 1. Узнайте какие телефоны из Kivano стоят столько же сколько и компьютеры из Sulpak.
+cursor.execute('SELECT kivano.product_name, sulpak.product_name, kivano.price, sulpak.price FROM kivano, sulpak WHERE kivano.category_id=1 AND sulpak.category_id=2 AND kivano.price=sulpak.price;')
+print(cursor.fetchall())
 # 3. Выведите на экран список всех ноутбуков из sulpak и только тех телефонов которые имеют
 # одинаковую дату выхода с компьютером из таблицы kivano.########################
 # 4. Выведите все китайские продукты.
 cursor.execute("select * from produsers where produser_country='China' ;")
 print('4 задание - ',cursor.fetchall())
-# 5. Напишите запрос, который выводит продукты любого магазина в порядке их добавления.
+cursor.execute('select * from produsers order by prodused_date;')
+print(cursor.fetchall())# 5. Напишите запрос, который выводит продукты
+#любого магазина в порядке их добавления.
 # 6. Найдите товары, которые есть в kivano но нет в sulpak.
+cursor.execute('select l.*, r.* from kivano l LEFT JOIN sulpak r ON r.product_name=l.product_name where r.product_name IS NULL;') 
+print(cursor.fetchall())
 # 7. Найдите все товары в магазине sulpak, где компания-производитель содержит букву "m" в имени.
+cursor.execute("SELECT product_name, produser_company FROM sulpak, produsers where produser_company LIKE '%m%';")
+print(cursor.fetchall())
+cursor.execute("SELECT product_name, produser_country FROM kivano, produsers where product_name LIKE '%k%' and produser_country='China';")
+print(cursor.fetchall())
 # 8. Найдите товары, которые есть и в kivano u sulpak.
-# 9. Найдите китайские товары из kivano, которые в названии содержат "k". 
+cursor.execute('''
+                select pr.produser_company, s.product_name
+                from produsers pr
+                join sulpak as s on pr.produser_id = s.item_id
+                where pr.produser_company like '%m%';
+                ''')
+print(f'товары, которые есть и в kivano u sulpak: \n{cursor.fetchall()}')
+# 9. Найдите китайские товары из kivano, которые в названии содержат "k".
+cursor.execute('''
+                select * from kivano
+                join produsers
+                on kivano.produser_id = produsers.produser_id
+                where produsers.produser_country = 'China' and kivano.product_name like '%k%'
+                ''')
+print(f'китайские товары из kivano, которые в названии содержат "k": \n{cursor.fetchall()}')
 # 10. Найдите самый последний добавленный товар в таблице produsers, и поменяйте компанию на Apple, и страну на kyrgyzstan.
 # 11. Нужно объеденить 2 магазина по product_name и вывести на экран имя продукта и его цену из обоих магазинов, однако не факт что в обоих магазинах будут одинаковые товары, поэтому нужно joinить по полной.
 # 12. Найдите самый последний японский товар который был добавлен в produsers.
 # 13. Напишите запрос, который прибавит 1000 к цене всех продуктов в sulpak.
 # 14. Узнать разницу между самой высокой ценой в sulpak и самой низкой ценой товар в kivano.
 # 15. Выведите на экран цены самых дешёвых телефонов из обоих магазинов.
+cursor.execute('''
+                select sulpak.price, kivano.price  from sulpak, kivano
+                order by sulpak.price, kivano.price limit 10;
+                ''')
+print(f'цены самых дешёвых телефонов: \n{cursor.fetchall()}')
 # 16. Удалить все записи где есть NULL в product_name в обоих магазинах.
 ######################### 17. Все телефоны у которых год меньше 1998 изменить на 2000 (Выполнить с помощью psycopg2).########################
 # 18. Acer закрыл свою фабрику в Бразилии после 2012 года и переехал в Германию, у всех записей в produsers где Acer был произведен в Brazil после 2012 поставьте Germany.
@@ -197,7 +227,11 @@ print('4 задание - ',cursor.fetchall())
 # 23. Найдите товар который лежит посередине в таблице sulpak.
 cursor.execute(" select * from sulpak where item_id=50;")
 print('23 задание - ',cursor.fetchall())
-
+# 23. Найдите товар который лежит посередине в таблице sulpak.
+cursor.execute('''
+                select * from sulpak where item_id = (select count(*) /2 from sulpak);
+                ''')
+print(f'товар который лежит посередине в таблице sulpak: \n{cursor.fetchall()}')
 # 24. Поменяйте страну на South Korea везде где страна Korea и компания Asus.
 # 25. В producers поменяйте Nokia на Microsoft везде где у компании Nokia указана страна USA.
 
